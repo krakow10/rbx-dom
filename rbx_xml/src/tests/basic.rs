94f148f881ddc03ba27649d32e828ca7c8452122
Quaternions <krakow20@gmail.com>
2025-04-28 13:48:17 -0700

    rbx_xml: port from ustr to hash_str

--- rbx_xml/src/tests/basic.rs
+++ rbx_xml/src/tests/basic.rs
@@ -6,7 +6,7 @@ use rbx_dom_weak::types::{
     NumberSequenceKeypoint, Rect, Tags, TerrainMaterials, UDim, UDim2, UniqueId, Variant,
     VariantType, Vector2, Vector3,
 };
-use rbx_dom_weak::{ustr, InstanceBuilder, WeakDom};
+use rbx_dom_weak::{hstr, InstanceBuilder, WeakDom};
 
 #[test]
 fn with_bool() {
@@ -30,7 +30,7 @@ fn with_bool() {
     assert_eq!(child.name, "BoolValue");
     assert_eq!(child.class, "BoolValue");
     assert_eq!(
-        child.properties.get(&"Value".into()),
+        child.properties.get(hstr!("Value")),
         Some(&Variant::Bool(true))
     );
 }
@@ -57,7 +57,7 @@ fn read_tags() {
     tags.push("World");
 
     assert_eq!(
-        folder.properties.get(&"Tags".into()),
+        folder.properties.get(hstr!("Tags")),
         Some(&Variant::Tags(tags))
     );
 }
@@ -115,8 +115,8 @@ fn read_attributes() {
     let dom = crate::from_str_default(document).unwrap();
     let folder = dom.get_by_ref(dom.root().children()[0]).unwrap();
 
-    assert_eq!(folder.properties.get(&"AttributesSerialize".into()), None);
-    let folder_attributes = match folder.properties.get(&"Attributes".into()) {
+    assert_eq!(folder.properties.get(hstr!("AttributesSerialize")), None);
+    let folder_attributes = match folder.properties.get(hstr!("Attributes")) {
         Some(Variant::Attributes(attrs)) => attrs,
         Some(other) => panic!(
             "Attributes property was not Attributes, it was: {:?}",
@@ -226,8 +226,7 @@ fn read_material_colors() {
     let dom = crate::from_str_default(document).unwrap();
     let terrain = dom.get_by_ref(dom.root().children()[0]).unwrap();
 
-    if let Some(Variant::MaterialColors(colors)) = terrain.properties.get(&"MaterialColors".into())
-    {
+    if let Some(Variant::MaterialColors(colors)) = terrain.properties.get(hstr!("MaterialColors")) {
         // There are tests to ensure competency in the actual MaterialColors
         // implementation, so these are just basic "are you ok" checks.
         assert_eq!(
@@ -245,7 +244,7 @@ fn read_material_colors() {
     } else {
         panic!(
             "MaterialColors was not Some(Variant::MaterialColors(_)) and was instead {:?}",
-            terrain.properties.get(&"MaterialColors".into())
+            terrain.properties.get(hstr!("MaterialColors"))
         )
     }
 }
@@ -283,7 +282,7 @@ fn read_unique_id() {
     assert_eq!(child.class, "Workspace");
 
     assert_eq!(
-        child.properties.get(&"UniqueId".into()),
+        child.properties.get(hstr!("UniqueId")),
         Some(&Variant::UniqueId(UniqueId::new(
             0x0048_15fc,
             0x02e9_c68d,
@@ -314,13 +313,13 @@ fn number_widening() {
     let int_value = tree.get_by_ref(tree.root().children()[0]).unwrap();
     assert_eq!(int_value.class, "IntValue");
     assert_eq!(
-        int_value.properties.get(&"Value".into()),
+        int_value.properties.get(hstr!("Value")),
         Some(&Variant::Int64(194))
     );
     let float_value = tree.get_by_ref(tree.root().children()[1]).unwrap();
     assert_eq!(float_value.class, "NumberValue");
     assert_eq!(
-        float_value.properties.get(&"Value".into()),
+        float_value.properties.get(hstr!("Value")),
         Some(&Variant::Float64(1337.0))
     );
 }
@@ -371,7 +370,7 @@ fn enum_item_to_enum() {
         .get_by_ref(*decoded.root().children().first().unwrap())
         .unwrap()
         .properties
-        .get(&ustr("Material"))
+        .get(hstr!("Material"))
         .unwrap()
         .ty();
 
