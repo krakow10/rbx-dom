5e5c90f3b6cdc00643435b8ff99f730a68a95e48
Quaternions <krakow20@gmail.com>
2025-05-03 14:34:23 -0700

    rbx_binary: use use entry to avoid duplicated work

--- rbx_binary/src/serializer/state.rs
+++ rbx_binary/src/serializer/state.rs
@@ -1,6 +1,6 @@
 use std::{
     borrow::{Borrow, Cow},
-    collections::{btree_map, BTreeMap},
+    collections::BTreeMap,
     convert::TryInto,
     io::Write,
 };
@@ -181,9 +181,14 @@ impl<'dom, 'db> TypeInfos<'dom, 'db> {
         database: &'db ReflectionDatabase<'db>,
         class: Ustr,
     ) -> &mut TypeInfo<'dom, 'db> {
-        if let btree_map::Entry::Vacant(entry) = self.values.entry(class) {
-            let type_id = self.next_type_id;
-            self.next_type_id += 1;
+        // Split self into independent mutable references.
+        let TypeInfos {
+            values,
+            next_type_id,
+        } = self;
+        values.entry(class).or_insert_with(|| {
+            let type_id = *next_type_id;
+            *next_type_id += 1;
 
             let class_descriptor = database.classes.get(class.as_str());
 
@@ -214,19 +219,15 @@ impl<'dom, 'db> TypeInfos<'dom, 'db> {
                 },
             );
 
-            entry.insert(TypeInfo {
+            TypeInfo {
                 type_id,
                 is_service,
                 instances: Vec::new(),
                 properties,
                 class_descriptor,
                 properties_visited: UstrSet::new(),
-            });
-        }
-
-        // This unwrap will not panic because we always insert this key into
-        // type_infos in this function.
-        self.values.get_mut(&class).unwrap()
+            }
+        })
     }
 }
 
