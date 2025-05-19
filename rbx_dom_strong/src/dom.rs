2439bbf438befedf5ff2942ccb49865fe1a7e8d2
Quaternions <krakow20@gmail.com>
2025-05-18 18:18:46 -0700

    rbx_dom_strong v1 via composition

---
+++ rbx_dom_strong/src/dom.rs
@@ -0,0 +1,12 @@
+use ahash::{AHashMap, AHashSet};
+use rbx_types::{Ref, UniqueId};
+
+use crate::instance::StrongInstance;
+
+/// Represents a DOM containing one or more Roblox instances.
+#[derive(Debug)]
+pub struct StrongDom {
+    instances: AHashMap<Ref, StrongInstance>,
+    root_ref: Ref,
+    unique_ids: AHashSet<UniqueId>,
+}
