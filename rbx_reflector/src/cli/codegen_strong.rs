f00b8d16aeff12550746d78c6658873bde0e74f5
Quaternions <krakow20@gmail.com>
2025-05-21 16:26:45 -0700

    instances codegen

--- rbx_reflector/src/cli/codegen_strong.rs
+++ rbx_reflector/src/cli/codegen_strong.rs
@@ -4,13 +4,70 @@ use std::process::{Command, Stdio};
 
 use clap::Parser;
 use quote::ToTokens;
-use rbx_reflection::{ClassDescriptor, EnumDescriptor};
+use rbx_reflection::{ClassDescriptor, DataType, EnumDescriptor};
 
 /// Generate strong types for all classes and enums.
 #[derive(Debug, Parser)]
 pub struct CodegenStrongSubcommand {
     /// Where to output the files.  This should be rbx_dom_strong/src/generated/
     pub output: PathBuf,
+}
+
+fn generate_data_type(data_type: &DataType) -> syn::Type {
+    match data_type {
+        DataType::Value(rbx_types::VariantType::Axes) => syn::parse_quote!(Axes),
+        DataType::Value(rbx_types::VariantType::BinaryString) => syn::parse_quote!(BinaryString),
+        DataType::Value(rbx_types::VariantType::Bool) => syn::parse_quote!(bool),
+        DataType::Value(rbx_types::VariantType::BrickColor) => syn::parse_quote!(BrickColor),
+        DataType::Value(rbx_types::VariantType::CFrame) => syn::parse_quote!(CFrame),
+        DataType::Value(rbx_types::VariantType::Color3) => syn::parse_quote!(Color3),
+        DataType::Value(rbx_types::VariantType::Color3uint8) => syn::parse_quote!(Color3uint8),
+        DataType::Value(rbx_types::VariantType::ColorSequence) => syn::parse_quote!(ColorSequence),
+        DataType::Value(rbx_types::VariantType::ContentId) => syn::parse_quote!(ContentId),
+        DataType::Value(rbx_types::VariantType::Enum) => syn::parse_quote!(Enum),
+        DataType::Value(rbx_types::VariantType::Faces) => syn::parse_quote!(Faces),
+        DataType::Value(rbx_types::VariantType::Float32) => syn::parse_quote!(f32),
+        DataType::Value(rbx_types::VariantType::Float64) => syn::parse_quote!(f64),
+        DataType::Value(rbx_types::VariantType::Int32) => syn::parse_quote!(i32),
+        DataType::Value(rbx_types::VariantType::Int64) => syn::parse_quote!(i64),
+        DataType::Value(rbx_types::VariantType::NumberRange) => syn::parse_quote!(NumberRange),
+        DataType::Value(rbx_types::VariantType::NumberSequence) => {
+            syn::parse_quote!(NumberSequence)
+        }
+        DataType::Value(rbx_types::VariantType::PhysicalProperties) => {
+            syn::parse_quote!(PhysicalProperties)
+        }
+        DataType::Value(rbx_types::VariantType::Ray) => syn::parse_quote!(Ray),
+        DataType::Value(rbx_types::VariantType::Rect) => syn::parse_quote!(Rect),
+        DataType::Value(rbx_types::VariantType::Ref) => syn::parse_quote!(Ref),
+        DataType::Value(rbx_types::VariantType::Region3) => syn::parse_quote!(Region3),
+        DataType::Value(rbx_types::VariantType::Region3int16) => syn::parse_quote!(Region3int16),
+        DataType::Value(rbx_types::VariantType::SharedString) => syn::parse_quote!(SharedString),
+        DataType::Value(rbx_types::VariantType::String) => syn::parse_quote!(String),
+        DataType::Value(rbx_types::VariantType::UDim) => syn::parse_quote!(UDim),
+        DataType::Value(rbx_types::VariantType::UDim2) => syn::parse_quote!(UDim2),
+        DataType::Value(rbx_types::VariantType::Vector2) => syn::parse_quote!(Vector2),
+        DataType::Value(rbx_types::VariantType::Vector2int16) => syn::parse_quote!(Vector2int16),
+        DataType::Value(rbx_types::VariantType::Vector3) => syn::parse_quote!(Vector3),
+        DataType::Value(rbx_types::VariantType::Vector3int16) => syn::parse_quote!(Vector3int16),
+        DataType::Value(rbx_types::VariantType::OptionalCFrame) => {
+            syn::parse_quote!(Option<CFrame>)
+        }
+        DataType::Value(rbx_types::VariantType::Tags) => syn::parse_quote!(Tags),
+        DataType::Value(rbx_types::VariantType::Attributes) => syn::parse_quote!(Attributes),
+        DataType::Value(rbx_types::VariantType::Font) => syn::parse_quote!(Font),
+        DataType::Value(rbx_types::VariantType::UniqueId) => syn::parse_quote!(UniqueId),
+        DataType::Value(rbx_types::VariantType::MaterialColors) => {
+            syn::parse_quote!(MaterialColors)
+        }
+        DataType::Value(rbx_types::VariantType::SecurityCapabilities) => {
+            syn::parse_quote!(SecurityCapabilities)
+        }
+        DataType::Value(rbx_types::VariantType::EnumItem) => syn::parse_quote!(EnumItem),
+        DataType::Value(rbx_types::VariantType::Content) => syn::parse_quote!(Content),
+        DataType::Enum(cow) => syn::parse_str(cow).unwrap(),
+        _ => unimplemented!(),
+    }
 }
 
 struct StrongInstancesCollector {
@@ -25,7 +82,71 @@ impl StrongInstancesCollector {
         }
     }
     fn push(&mut self, descriptor: &ClassDescriptor) {
-        // TODO
+        // generate fields
+        let mut fields = Vec::new();
+        for prop in descriptor.properties.values() {
+            let ident = syn::Ident::new(&prop.name, proc_macro2::Span::call_site());
+            let ty = generate_data_type(&prop.data_type);
+            let field = syn::Field {
+                attrs: Vec::new(),
+                vis: syn::Visibility::Public(syn::token::Pub::default()),
+                mutability: syn::FieldMutability::None,
+                ident: Some(ident),
+                colon_token: Some(syn::token::Colon::default()),
+                ty,
+            };
+            fields.push(field);
+        }
+        // sort props for consistency
+        fields.sort_by(|a, b| a.ident.cmp(&b.ident));
+
+        // struct ident
+        let ident = syn::Ident::new(&descriptor.name, proc_macro2::Span::call_site());
+
+        // generate the class struct
+        self.structs.push(syn::ItemStruct {
+            attrs: vec![syn::parse_quote!(#[derive(Debug, Clone, Serialize, Deserialize)])],
+            vis: syn::Visibility::Public(syn::token::Pub::default()),
+            struct_token: syn::token::Struct::default(),
+            ident: ident.clone(),
+            generics: syn::Generics::default(),
+            fields: syn::Fields::Named(syn::FieldsNamed {
+                brace_token: syn::token::Brace::default(),
+                named: fields.into_iter().collect(),
+            }),
+            semi_token: None,
+        });
+
+        // generate the StrongInstances variant
+        self.variants.push(syn::Variant {
+            attrs: Vec::new(),
+            ident: ident.clone(),
+            fields: syn::Fields::Unnamed(syn::FieldsUnnamed {
+                paren_token: syn::token::Paren::default(),
+                unnamed: [syn::Field {
+                    attrs: vec![],
+                    vis: syn::Visibility::Inherited,
+                    mutability: syn::FieldMutability::None,
+                    ident: None,
+                    colon_token: None,
+                    ty: syn::Type::Path(syn::TypePath {
+                        qself: None,
+                        path: syn::Path {
+                            leading_colon: None,
+                            segments: [syn::PathSegment {
+                                ident,
+                                arguments: syn::PathArguments::None,
+                            }]
+                            .into_iter()
+                            .collect(),
+                        },
+                    }),
+                }]
+                .into_iter()
+                .collect(),
+            }),
+            discriminant: None,
+        });
     }
     fn codegen(mut self) -> syn::File {
         // sort for consistency
