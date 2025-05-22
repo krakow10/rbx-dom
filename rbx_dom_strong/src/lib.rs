c657a92473e36982250e107d8dcd0ce517c15abd
Quaternions <krakow20@gmail.com>
2025-05-21 21:52:58 -0700

    drop inner module

--- rbx_dom_strong/src/lib.rs
+++ rbx_dom_strong/src/lib.rs
@@ -1,2 +1,4 @@
 mod dom;
+
+#[cfg(test)]
 mod test;
--- rbx_dom_strong/src/test.rs
+++ rbx_dom_strong/src/test.rs
@@ -1,22 +1,19 @@
-#[cfg(test)]
-mod test {
-    use rbx_types_strong::instances::Part;
-    use rbx_types::{CFrame, Matrix3, Vector3};
+use rbx_types::{CFrame, Matrix3, Vector3};
+use rbx_types_strong::instances::Part;
 
-    fn default_cframe() -> CFrame {
-        CFrame::new(Vector3::new(0.0, 0.0, 0.0), Matrix3::identity())
-    }
-    fn default_part() -> Part {
-        // currently no way to initialize
-        todo!()
-    }
-    #[test]
-    fn part_inherits_instance() {
-        // Part::default() would be nice
-        let mut part: Part = default_part();
+fn default_cframe() -> CFrame {
+    CFrame::new(Vector3::new(0.0, 0.0, 0.0), Matrix3::identity())
+}
+fn default_part() -> Part {
+    // currently no way to initialize
+    todo!()
+}
+#[test]
+fn part_inherits_instance() {
+    // Part::default() would be nice
+    let mut part: Part = default_part();
 
-        // look ma, inheritance in rust!
-        part.Name = "Part".to_owned();
-        part.CFrame = default_cframe();
-    }
+    // look ma, inheritance in rust!
+    part.Name = "Part".to_owned();
+    part.CFrame = default_cframe();
 }
