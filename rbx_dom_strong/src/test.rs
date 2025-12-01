e767b38cc8b27f53179eaf4b1affb26245cbeceb
Rhys Lloyd <krakow20@gmail.com>
2025-12-01 15:01:14 -0800

    fixup test

--- rbx_dom_strong/src/test.rs
+++ rbx_dom_strong/src/test.rs
@@ -1,19 +1,11 @@
-use rbx_types::{CFrame, Matrix3, Vector3};
+use rbx_types::CFrame;
 use rbx_types_strong::instances::Part;
 
-fn default_cframe() -> CFrame {
-    CFrame::new(Vector3::new(0.0, 0.0, 0.0), Matrix3::identity())
-}
-fn default_part() -> Part {
-    // currently no way to initialize
-    todo!()
-}
 #[test]
 fn part_inherits_instance() {
-    // Part::default() would be nice
-    let mut part: Part = default_part();
+    let mut part = Part::default();
 
     // look ma, inheritance in rust!
     part.Name = "Part".to_owned();
-    part.CFrame = default_cframe();
+    part.CFrame = CFrame::identity();
 }
