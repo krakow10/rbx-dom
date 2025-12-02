58d2b9365e725aab1030fd7d3fcce08ea8d1b34f
Rhys Lloyd <krakow20@gmail.com>
2025-12-02 10:21:38 -0800

    example

--- rbx_types_strong/src/traits.rs
+++ rbx_types_strong/src/traits.rs
@@ -5,3 +5,19 @@ pub trait Properties{
     fn set_property(&mut self,prop_name:&str,value:Variant);
 }
 
+impl Properties for crate::instances::BasePart{
+    fn clone_property(&self,prop_name:&str)->Variant {
+        match prop_name{
+            "CFrame"=>Variant::CFrame(self.CFrame),
+            // other=>self.superclass.clone_property(other),
+            other=>panic!("unknown property {other}"),
+        }
+    }
+    fn set_property(&mut self,prop_name:&str,value:Variant) {
+        match (prop_name,value){
+           ("CFrame",Variant::CFrame(value))=>self.CFrame=value,
+           // (other,variant)=>self.superclass.set_property(other,variant),
+           (other,variant)=>panic!("unknown property '{other}' or wrong variant type '{variant:?}'"),
+        }
+    }
+}
