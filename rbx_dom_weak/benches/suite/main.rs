903052ca88bda734947e0f810f67a39d46ab74b6
Rhys Lloyd <krakow20@gmail.com>
2025-12-06 11:19:22 -0800

    bench miners haven

--- rbx_dom_weak/benches/suite/main.rs
+++ rbx_dom_weak/benches/suite/main.rs
@@ -9,26 +9,18 @@ pub fn clone_into_external(c: &mut Criterion) {
     group
         // cranking sample size and measurement time to ensure we get a
         // statistically significant result
-        .sample_size(1000)
         .measurement_time(Duration::from_secs(30))
-        .bench_function("1000 ref rewrites", |b| {
+        .bench_function("Miner's Haven", |b| {
             b.iter_batched(
                 || {
-                    let mut dest_tree = WeakDom::new(InstanceBuilder::new("Folder"));
-                    dest_tree.reserve(1000);
+                    let dest_tree = WeakDom::new(InstanceBuilder::new("Folder"));
 
-                    let mut src_tree = WeakDom::new(InstanceBuilder::new("Folder"));
-                    let src_instance = InstanceBuilder::new("Folder");
-                    let src_referent = src_instance.referent();
-                    let mut children = Vec::with_capacity(1000);
+                    let miners_haven =
+                        include_bytes!("../../../rbx_binary/benches/files/miners-haven.rbxl")
+                            .as_slice();
+                    let src_tree = rbx_binary::from_reader(miners_haven).unwrap();
+                    let src_referent = src_tree.root_ref();
 
-                    for _ in 0..1000 {
-                        children.push(
-                            InstanceBuilder::new("ObjectValue")
-                                .with_property("Value", src_referent),
-                        );
-                    }
-                    src_tree.insert(src_tree.root_ref(), src_instance.with_children(children));
                     (src_referent, src_tree, dest_tree)
                 },
                 |(src_instance, src_tree, mut dest_tree)| {
