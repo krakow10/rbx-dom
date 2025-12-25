59bb5636398a04a1b2ae5a4c06b79e8a925ed61f
Quaternions <krakow20@gmail.com>
2025-12-25 14:15:49 -0800

    combine tests

--- rbx_dom_strong/src/test.rs
+++ rbx_dom_strong/src/test.rs
@@ -1,26 +1,19 @@
 use crate::dom::StrongDom;
-use crate::instances::{BasePart, Part};
+use crate::instances::Part;
 use rbx_types::{CFrame, Ref};
 
 #[test]
 fn part_inherits_instance() {
-    let mut part = Part::default();
-
-    // look ma, inheritance in rust!
-    part.Name = "Part".to_owned();
-    part.CFrame = CFrame::identity();
-}
-
-#[test]
-fn get_by_ref() {
-    let dom = StrongDom::default();
+    let mut dom = StrongDom::default();
 
     // dummy referent just to get this compiling...
     let referent = Ref::none();
 
-    if let Some(instance) = dom.get_by_ref(referent)
-        && let Some(base_part) = instance.as_class::<BasePart>()
+    if let Some(instance) = dom.get_by_ref_mut(referent)
+        && let Some(part) = instance.as_class_mut::<Part>()
     {
-        println!("BasePart is a superclass {:?}", base_part.CFrame)
+        // look ma, inheritance in rust!
+        part.Name = "Part".to_owned();
+        part.CFrame = CFrame::identity();
     }
 }
