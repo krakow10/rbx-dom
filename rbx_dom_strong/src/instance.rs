0146fb3022cf8ec189db07a71d136d9687fff491
Rhys Lloyd <krakow20@gmail.com>
2025-12-26 12:06:14 -0800

    shuffle code around

--- rbx_dom_strong/src/instance.rs
+++ rbx_dom_strong/src/instance.rs
@@ -1,13 +1,62 @@
 use rbx_classes::instances;
 use rbx_types::Ref;
 
-#[derive(Debug)]
-pub struct Instance {
-    referent: Ref,
-    children: Vec<Ref>,
-    parent: Ref,
-    class: Class,
+macro_rules! impl_class {
+    ($($class:ident),*) => {
+        #[derive(Debug)]
+        pub enum Class {
+            $(
+                $class(Box<instances::$class>),
+            )*
+        }
+
+        // From impls
+        $(
+            impl From<instances::$class> for Class {
+                fn from(value: instances::$class) -> Self {
+                    Self::$class(Box::new(value))
+                }
+            }
+        )*
+    };
 }
+rbx_classes::for_each_class!(impl_class);
+
+pub trait AsClass<Class> {
+    fn as_class(&self) -> Option<&Class>;
+    fn as_class_mut(&mut self) -> Option<&mut Class>;
+}
+macro_rules! impl_as_class_for_instance_and_descendants {
+    ($class:ident, [$($descendant:ident),*]) => {
+        #[allow(unreachable_patterns)]
+        impl AsClass<instances::$class> for Class {
+            fn as_class(&self) -> Option<&instances::$class> {
+                Some(match self {
+                    $(
+                        Class::$descendant(class) => class,
+                    )*
+                    _ => return None,
+                })
+            }
+            fn as_class_mut(&mut self) -> Option<&mut instances::$class> {
+                Some(match self {
+                    $(
+                        Class::$descendant(class) => class,
+                    )*
+                    _ => return None,
+                })
+            }
+        }
+    };
+}
+macro_rules! impl_as_class {
+    ($(($class:ident,$descendants:tt)),*) => {
+        $(
+            impl_as_class_for_instance_and_descendants!($class,$descendants);
+        )*
+    };
+}
+rbx_classes::for_each_class_descendants!(impl_as_class);
 
 #[derive(Debug)]
 pub struct InstanceBuilder<C> {
@@ -68,62 +117,13 @@ impl<C> core::ops::DerefMut for InstanceBuilder<C> {
     }
 }
 
-macro_rules! impl_strong_instance {
-    ($($class:ident),*) => {
-        #[derive(Debug)]
-        pub enum Class {
-            $(
-                $class(Box<instances::$class>),
-            )*
-        }
-
-        // From impls
-        $(
-            impl From<instances::$class> for Class {
-                fn from(value: instances::$class) -> Self {
-                    Self::$class(Box::new(value))
-                }
-            }
-        )*
-    };
+#[derive(Debug)]
+pub struct Instance {
+    referent: Ref,
+    children: Vec<Ref>,
+    parent: Ref,
+    class: Class,
 }
-rbx_classes::for_each_class!(impl_strong_instance);
-
-pub trait AsClass<Class> {
-    fn as_class(&self) -> Option<&Class>;
-    fn as_class_mut(&mut self) -> Option<&mut Class>;
-}
-macro_rules! impl_as_class_for_instance_and_descendants {
-    ($class:ident, [$($descendant:ident),*]) => {
-        #[allow(unreachable_patterns)]
-        impl AsClass<instances::$class> for Class {
-            fn as_class(&self) -> Option<&instances::$class> {
-                Some(match self {
-                    $(
-                        Class::$descendant(class) => class,
-                    )*
-                    _ => return None,
-                })
-            }
-            fn as_class_mut(&mut self) -> Option<&mut instances::$class> {
-                Some(match self {
-                    $(
-                        Class::$descendant(class) => class,
-                    )*
-                    _ => return None,
-                })
-            }
-        }
-    };
-}
-macro_rules! impl_as_class {
-    ($(($class:ident,$descendants:tt)),*) => {
-        $(
-            impl_as_class_for_instance_and_descendants!($class,$descendants);
-        )*
-    };
-}
-rbx_classes::for_each_class_descendants!(impl_as_class);
 
 impl Instance {
     /// Returns this instance's referent. It will always be non-null.
