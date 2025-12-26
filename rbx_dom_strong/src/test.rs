cb3bf3ccc1cdba0a91757528cf091351c3059614
Quaternions <krakow20@gmail.com>
2025-12-25 17:43:50 -0800

    test test

--- rbx_dom_strong/src/test.rs
+++ rbx_dom_strong/src/test.rs
@@ -7,13 +7,24 @@ type PartBuilder = instances::Part<InstanceBuilderInner>;
 
 #[test]
 fn builder() {
+    use crate::instances::Part;
     let mut part_builder = PartBuilder::default();
 
     part_builder.Name = "Part".to_owned();
     part_builder.CFrame = CFrame::identity();
     part_builder.Anchored = true;
 
+    let referent = part_builder.referent();
+
     let dom = StrongDom::new(part_builder);
+
+    let part = dom
+        .get_by_ref(referent)
+        .unwrap()
+        .as_class::<Part>()
+        .unwrap();
+
+    assert_eq!(part.Name, "Part");
 }
 
 #[test]
