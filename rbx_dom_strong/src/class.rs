54049658b6625274d535a342a5dde847e93ef26e
Rhys Lloyd <krakow20@gmail.com>
2026-06-09 12:08:02 -0700

    add rbx_dom_strong crate

---
+++ rbx_dom_strong/src/class.rs
@@ -0,0 +1,87 @@
+use rbx_classes::instances;
+
+macro_rules! impl_class {
+    ($($class:ident),*) => {
+        /// Class is an enum of boxed structs,
+        /// with each struct being a different class.
+        // ReflectionMetadataClass annoyingly procs this lint
+        #[expect(clippy::enum_variant_names)]
+        #[derive(Debug)]
+        pub enum Class {
+            $(
+                $class(Box<instances::$class>),
+            )*
+        }
+
+        // From impls
+        $(
+            impl From<instances::$class> for Class {
+                fn from(value: instances::$class) -> Self {
+                    Self::$class(Box::new(value))
+                }
+            }
+        )*
+    };
+}
+rbx_classes::for_each_class!(impl_class);
+
+/// Convert a `Class` enum into any descendant
+/// of a superclass using dereferencing.
+pub trait AsClass<C> {
+    fn as_class(&self) -> Option<&C>;
+    fn as_class_mut(&mut self) -> Option<&mut C>;
+}
+/// Class to class conversion. Inheritance as a trait bound.
+/// Perform an infallible conversion using dereferencing.
+///
+/// Part -> BasePart is possible, but BasePart -> Part is not.
+///
+/// If you have the class, you can simply use dereferencing directly,
+/// but if you have a generic parameter that accepts a class,
+/// this can be used as a trait bound to access the properties
+/// of a particular class.
+pub trait ToClass<C> {
+    fn to_class(&self) -> &C;
+    fn to_class_mut(&mut self) -> &mut C;
+}
+macro_rules! impl_traits_for_class_and_descendants {
+    ($class:ident, [$($descendant:ident),*]) => {
+        #[allow(unreachable_patterns)]
+        impl AsClass<instances::$class> for Class {
+            fn as_class(&self) -> Option<&instances::$class> {
+                Some(match self {
+                    $(
+                        Class::$descendant(class) => class,
+                    )*
+                    _ => return None,
+                })
+            }
+            fn as_class_mut(&mut self) -> Option<&mut instances::$class> {
+                Some(match self {
+                    $(
+                        Class::$descendant(class) => class,
+                    )*
+                    _ => return None,
+                })
+            }
+        }
+        $(
+            impl ToClass<instances::$class> for instances::$descendant {
+                fn to_class(&self) -> &instances::$class {
+                    self
+                }
+                fn to_class_mut(&mut self) -> &mut instances::$class {
+                    self
+                }
+            }
+        )*
+    };
+}
+macro_rules! impl_as_class {
+    ($(($class:ident,$descendants:tt)),*) => {
+        $(
+            impl_traits_for_class_and_descendants!($class,$descendants);
+        )*
+    };
+}
+rbx_classes::for_each_class_descendants!(impl_as_class);
---
+++ rbx_dom_strong/src/instance.rs
@@ -0,0 +1,200 @@
+use rbx_types::Ref;
+
+use crate::class::{AsClass, Class, ToClass};
+
+#[derive(Debug)]
+pub struct InstanceBuilder<C> {
+    referent: Ref,
+    children: Vec<InstanceBuilder<Class>>,
+    class: C,
+}
+
+impl<C> InstanceBuilder<C> {
+    /// Create a new `InstanceBuilder` with the given Class.
+    pub fn new(class: C) -> Self {
+        InstanceBuilder {
+            referent: Ref::new(),
+            children: Vec::new(),
+            class,
+        }
+    }
+
+    /// Return the referent of the instance that the `InstanceBuilder` refers to.
+    pub fn referent(&self) -> Ref {
+        self.referent
+    }
+
+    /// Change the referent of the `InstanceBuilder`.
+    pub fn with_referent<R: Into<Ref>>(self, referent: R) -> Self {
+        Self {
+            referent: referent.into(),
+            ..self
+        }
+    }
+
+    /// Change the referent of the `InstanceBuilder`.
+    pub fn set_referent<R: Into<Ref>>(&mut self, referent: R) {
+        self.referent = referent.into();
+    }
+
+    /// Change the name of the `InstanceBuilder`.
+    pub fn with_name<S: Into<String>>(mut self, name: S) -> Self
+    where
+        C: ToClass<rbx_classes::instances::Instance>,
+    {
+        self.set_name(name);
+        self
+    }
+
+    /// Change the name of the `InstanceBuilder`.
+    pub fn set_name<S: Into<String>>(&mut self, name: S)
+    where
+        C: ToClass<rbx_classes::instances::Instance>,
+    {
+        self.class.to_class_mut().Name = name.into();
+    }
+
+    /// Add a new child to the `InstanceBuilder`.
+    pub fn with_child<ChildClass>(mut self, child: InstanceBuilder<ChildClass>) -> Self
+    where
+        ChildClass: Into<Class>,
+    {
+        self.children.push(child.into_class());
+        self
+    }
+
+    /// Add a new child to the `InstanceBuilder`.
+    pub fn add_child<ChildClass>(&mut self, child: InstanceBuilder<ChildClass>)
+    where
+        ChildClass: Into<Class>,
+    {
+        self.children.push(child.into_class());
+    }
+
+    /// Add multiple children to the `InstanceBuilder` at once.
+    ///
+    /// Order of the children will be preserved.
+    pub fn with_children<I>(mut self, children: I) -> Self
+    where
+        I: IntoIterator<Item = InstanceBuilder<Class>>,
+    {
+        self.children.extend(children);
+        self
+    }
+
+    /// Add multiple children to the `InstanceBuilder` at once.
+    ///
+    /// Order of the children will be preserved.
+    pub fn add_children<I>(&mut self, children: I)
+    where
+        I: IntoIterator<Item = InstanceBuilder<Class>>,
+    {
+        self.children.extend(children);
+    }
+
+    pub(crate) fn children(&self) -> &[InstanceBuilder<Class>] {
+        &self.children
+    }
+
+    pub(crate) fn into_class(self) -> InstanceBuilder<Class>
+    where
+        C: Into<Class>,
+    {
+        InstanceBuilder {
+            referent: self.referent,
+            children: self.children,
+            class: self.class.into(),
+        }
+    }
+}
+
+impl<C: Default> Default for InstanceBuilder<C> {
+    fn default() -> Self {
+        Self {
+            referent: Ref::new(),
+            children: Vec::new(),
+            class: C::default(),
+        }
+    }
+}
+impl<C> core::ops::Deref for InstanceBuilder<C> {
+    type Target = C;
+    fn deref(&self) -> &Self::Target {
+        &self.class
+    }
+}
+impl<C> core::ops::DerefMut for InstanceBuilder<C> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.class
+    }
+}
+
+#[derive(Debug)]
+pub struct Instance {
+    referent: Ref,
+    children: Vec<Ref>,
+    parent: Ref,
+    class: Class,
+}
+
+impl Instance {
+    /// Returns this instance's referent. It will always be non-null.
+    #[inline]
+    pub fn referent(&self) -> Ref {
+        self.referent
+    }
+
+    /// Returns a list of the referents corresponding to the instance's
+    /// children. All referents returned will be non-null and point to valid
+    /// instances in the same [`WeakDom`][crate::WeakDom].
+    #[inline]
+    pub fn children(&self) -> &[Ref] {
+        &self.children
+    }
+
+    /// Returns the referent corresponding to this instance's parent. This
+    /// referent will either point to an instance in the same
+    /// [`WeakDom`][crate::WeakDom] or be null.
+    #[inline]
+    pub fn parent(&self) -> Ref {
+        self.parent
+    }
+
+    /// Cast the instance's class as a superclass.  For example, if the
+    /// Instance's Class is Part, `instance.as_class::<BasePart>()` gives
+    /// `Some(&BasePart)`.
+    pub fn as_class<C>(&self) -> Option<&C>
+    where
+        Class: AsClass<C>,
+    {
+        self.class.as_class()
+    }
+
+    /// Cast the instance's class as a superclass.  For example, if the
+    /// Instance's Class is Part, `instance.as_class_mut::<BasePart>()` gives
+    /// `Some(&mut BasePart)`.
+    pub fn as_class_mut<C>(&mut self) -> Option<&mut C>
+    where
+        Class: AsClass<C>,
+    {
+        self.class.as_class_mut()
+    }
+
+    pub(crate) fn children_mut(&mut self) -> &mut Vec<Ref> {
+        &mut self.children
+    }
+    pub(crate) fn from_builder<C: Into<Class>>(
+        parent: Ref,
+        builder: InstanceBuilder<C>,
+    ) -> (Self, Vec<InstanceBuilder<Class>>) {
+        (
+            Instance {
+                referent: builder.referent,
+                children: Vec::with_capacity(builder.children.len()),
+                parent,
+                class: builder.class.into(),
+            },
+            builder.children,
+        )
+    }
+}
