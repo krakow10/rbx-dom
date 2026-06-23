c77debf2ef82f89eb83780c7d08c41bd65495e32
Rhys Lloyd <krakow20@gmail.com>
2026-06-23 16:13:53 -0700

    Reapply "do not pre-allocate"
    This reverts commit d841407ff9600a9dfe0fa5c6e165fca62eb6618a.

--- rbx_binary/src/deserializer/rayon_transpose.rs
+++ rbx_binary/src/deserializer/rayon_transpose.rs
@@ -1,6 +1,7 @@
 use std::collections::HashMap;
 use std::hash::{BuildHasher, Hash};
 use std::iter;
+use std::marker::PhantomData;
 
 use rayon::iter::plumbing::{bridge, Consumer, Producer, ProducerCallback, UnindexedConsumer};
 use rayon::iter::{IndexedParallelIterator, ParallelIterator};
@@ -24,8 +25,8 @@ impl<K, V, S> HashMapVecTranspose<K, V, S> {
 impl<K, V, S> ParallelIterator for HashMapVecTranspose<K, V, S>
 where
     K: Send + Clone + Eq + Hash,
-    V: Send + Clone,
-    S: Send + Clone + BuildHasher + Default,
+    V: Send,
+    S: Send + BuildHasher + Default,
 {
     type Item = HashMap<K, V, S>;
 
@@ -41,8 +42,8 @@ where
 impl<K, V, S> IndexedParallelIterator for HashMapVecTranspose<K, V, S>
 where
     K: Send + Clone + Eq + Hash,
-    V: Send + Clone,
-    S: Send + Clone + BuildHasher + Default,
+    V: Send,
+    S: Send + BuildHasher + Default,
 {
     fn drive<C: Consumer<Self::Item>>(self, consumer: C) -> C::Result {
         bridge(self, consumer)
@@ -53,14 +54,8 @@ where
     }
 
     fn with_producer<CB: ProducerCallback<Self::Item>>(mut self, callback: CB) -> CB::Output {
-        // Allocate the HashMaps single-threaded to avoid allocator lock contention
-        let mut hash_maps = vec![];
-        hash_maps.resize_with(self.len, || {
-            HashMap::with_capacity_and_hasher(self.map.len(), S::default())
-        });
-
         // Create the producer as the exclusive "owner" of the slice.
-        let producer = TransposeProducer::from_transpose(&mut self, &mut hash_maps);
+        let producer = TransposeProducer::from_transpose(&mut self);
 
         // The producer will move or drop each item from the drained range.
         callback.callback(producer)
@@ -68,28 +63,26 @@ where
 }
 
 // ////////////////////////////////////////////////////////////////////////
-struct TransposeProducer<'data, K: Send, V: Send, S: Send> {
+struct TransposeProducer<'data, K, V: Send, S> {
     map: Vec<(K, DrainProducer<'data, V>)>,
-    hash_maps: DrainProducer<'data, HashMap<K, V, S>>,
+    len: usize,
+    _randomstate: PhantomData<S>,
 }
 
 impl<'data, K, V, S> TransposeProducer<'data, K, V, S>
 where
-    K: Send + Clone + Eq + Hash,
+    K: Clone + Eq + Hash,
     V: Send,
-    S: Send + BuildHasher + Default,
 {
-    fn new(
-        map: Vec<(K, DrainProducer<'data, V>)>,
-        hash_maps: DrainProducer<'data, HashMap<K, V, S>>,
-    ) -> Self {
-        Self { map, hash_maps }
+    fn new(map: Vec<(K, DrainProducer<'data, V>)>, len: usize) -> Self {
+        Self {
+            map,
+            len,
+            _randomstate: PhantomData,
+        }
     }
 
-    fn from_transpose(
-        transpose: &'data mut HashMapVecTranspose<K, V, S>,
-        hash_maps: &'data mut Vec<HashMap<K, V, S>>,
-    ) -> Self {
+    fn from_transpose<S2>(transpose: &'data mut HashMapVecTranspose<K, V, S2>) -> Self {
         let len = transpose.len;
         let map = transpose
             .map
@@ -102,11 +95,7 @@ where
                 })
             })
             .collect();
-        let hash_maps = unsafe {
-            hash_maps.set_len(0);
-            DrainProducer::from_vec(hash_maps, len)
-        };
-        Self::new(map, hash_maps)
+        Self::new(map, len)
     }
 }
 
@@ -120,18 +109,22 @@ where
     type IntoIter = TransposeSliceDrain<'data, K, V, S>;
 
     fn into_iter(self) -> Self::IntoIter {
+        let len = self.len;
         let map = self
             .map
             .into_iter()
             .map(|(key, drain_producer)| (key, drain_producer.into_iter()))
             .collect();
-        let hash_maps = self.hash_maps.into_iter();
-        TransposeSliceDrain { map, hash_maps }
+        TransposeSliceDrain {
+            map,
+            len,
+            _randomstate: PhantomData,
+        }
     }
 
     fn split_at(self, index: usize) -> (Self, Self) {
         // TODO: figure out if there is a way to reuse the allocation of self
-        let (m_left, m_right) = self
+        let (left, right) = self
             .map
             .into_iter()
             .map(|(key, drain_producer)| {
@@ -139,8 +132,7 @@ where
                 ((key.clone(), left), (key, right))
             })
             .collect();
-        let (h_left, h_right) = self.hash_maps.split_at(index);
-        (Self::new(m_left, h_left), Self::new(m_right, h_right))
+        (Self::new(left, index), Self::new(right, self.len - index))
     }
 }
 
@@ -149,7 +141,8 @@ where
 // like std::vec::Drain, without updating a source Vec
 struct TransposeSliceDrain<'data, K, V, S> {
     map: Vec<(K, SliceDrain<'data, V>)>,
-    hash_maps: SliceDrain<'data, HashMap<K, V, S>>,
+    len: usize,
+    _randomstate: PhantomData<S>,
 }
 
 impl<'data, K, V, S> Iterator for TransposeSliceDrain<'data, K, V, S>
@@ -161,21 +154,18 @@ where
     type Item = HashMap<K, V, S>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        let mut hash_map = self.hash_maps.next()?;
-        hash_map.extend(
-            self.map
-                .iter_mut()
-                .map(|(key, iter)| (key.clone(), iter.next().unwrap())),
-        );
-        Some(hash_map)
+        self.map
+            .iter_mut()
+            .map(|(key, iter)| Some((key.clone(), iter.next()?)))
+            .collect()
     }
 
     fn size_hint(&self) -> (usize, Option<usize>) {
-        (self.len(), Some(self.len()))
+        (self.len, Some(self.len))
     }
 
     fn count(self) -> usize {
-        self.len()
+        self.len
     }
 }
 
@@ -186,13 +176,10 @@ where
     S: BuildHasher + Default,
 {
     fn next_back(&mut self) -> Option<Self::Item> {
-        let mut hash_map = self.hash_maps.next_back()?;
-        hash_map.extend(
-            self.map
-                .iter_mut()
-                .map(|(key, iter)| (key.clone(), iter.next_back().unwrap())),
-        );
-        Some(hash_map)
+        self.map
+            .iter_mut()
+            .map(|(key, iter)| Some((key.clone(), iter.next_back()?)))
+            .collect()
     }
 }
 
@@ -203,7 +190,7 @@ where
     S: BuildHasher + Default,
 {
     fn len(&self) -> usize {
-        self.hash_maps.len()
+        self.len
     }
 }
 
